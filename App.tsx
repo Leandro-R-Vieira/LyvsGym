e1a421ebbdf4f4db3f7e29963ec4b03c0c8b9b75
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-12-15 18:04:47 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+
+import { StatusBar, Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'; 
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  return (
+    <View >
+      <StatusBar 
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent
+      />     
+      {fontsLoaded ? <Text>Hello World!</Text> : <View />}      
+    </View>
+  );
+}
+
