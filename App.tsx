b7f35c60de5c7d3b1c05e7eacd7cf8853c59c3d1
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-12-26 19:27:46 -0300

    SVG

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
-
-import { StatusBar, Text, View } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'; 
+import { StatusBar, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -9,14 +12,14 @@ export default function App() {
   })
 
   return (
-    <View >
-      <StatusBar 
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
         backgroundColor="transparent"
         barStyle="light-content"
         translucent
-      />     
-      {fontsLoaded ? <Text>Hello World!</Text> : <View />}      
-    </View>
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
 
