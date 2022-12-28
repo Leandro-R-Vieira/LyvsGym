7b4ddaa569c0c52516c24c57ccb6e45997bead08
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-12-28 07:11:53 -0300

    userPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { HStack, Text, Heading, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto 
+        source={{uri: 'https://github.com/Leandro-R-Vieira.png'}} 
+        size={16} 
+        alt='Imagem do usuário' 
+        mr={4} 
+        />
       <VStack>
         <Text color='gray.100' fontSize='md'>Hi</Text>
         <Heading color='gray.100'fontSize='md'>Leandro</Heading>
