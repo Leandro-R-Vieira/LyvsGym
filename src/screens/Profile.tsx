c099ef8400971d8358160b32975a262940d77e16
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2023-01-05 12:01:10 -0300

    Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,21 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "native-base";
 
-export function Profile(){
-  return(
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+export function Profile() {
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{ uri: 'https://github.com/Leandro-R-Vieira.png' }}
+            alt='Foto do Usuário'
+            size={33}
+          />
+        </Center>
+      </ScrollView>
+
+    </VStack>
   )
 }
