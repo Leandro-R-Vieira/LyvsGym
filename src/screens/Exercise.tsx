f6185635feed6c4a283dc00177f19f6b4bc9179c
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2023-01-06 12:27:11 -0300

    goback profile button

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,23 @@
-import { Center, Text } from "native-base";
+import { Icon, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 export function Exercise(){
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack(){
+    navigation.goBack();
+  }
+
   return(
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg='gray.600' pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name='arrow-left' color='darkBlue.500' size={6}/>
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   )
 }
