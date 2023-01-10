5cf2f2982714242cf672fceb1968eebf0f13b101
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2023-01-10 09:26:59 -0300

    loading photos

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -10,10 +10,26 @@ import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base
 const PHOTO_SIZE = 33;
 
 export function Profile() {
-  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [photoIsLoading, setPhotoIsLoading] = useState<boolean>(false);
+  const [userPhoto, setUserPhoto] = useState<string | undefined>('https://github.com/Leandro-R-Vieira.png');
 
-  async function handleUserPhotoSelect(){
-    await ImagePicker.launchImageLibraryAsync();
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true
+      });
+      if (photoSelected.canceled) return;
+      if(photoSelected.assets[0].uri)
+      setUserPhoto(photoSelected.assets[0].uri);
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false);
+    }
   }
 
   return (
@@ -31,7 +47,7 @@ export function Profile() {
             />
             :
             <UserPhoto
-              source={{ uri: 'https://github.com/Leandro-R-Vieira.png' }}
+              source={{ uri: userPhoto }}
               alt='Foto do Usuário'
               size={PHOTO_SIZE}
             />}
