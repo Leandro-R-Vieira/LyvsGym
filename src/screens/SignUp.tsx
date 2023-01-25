6a15d89e722375206762235747ab251366c89808
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2023-01-25 15:11:30 -0300

    axios errors

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,12 +1,15 @@
-import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { api } from '@services/api';
 import LogoSvg from '@assets/logo.svg';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import BackgroundImg from '@assets/background.png';
-import { useNavigation } from '@react-navigation/native';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import axios from 'axios';
+import { Alert } from 'react-native';
 
 type FormDataProps = {
   name: string;
@@ -16,10 +19,10 @@ type FormDataProps = {
 }
 
 const signUpSchema = yup.object({
-    name: yup.string().required('Informe o nome'),
-    email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
-    password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo mneos 6 dígitos'),
-    password_confirm: yup.string().required('Confirme a senha').oneOf([yup.ref('password'), null], 'A confirmação da senha não confere')
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo mneos 6 dígitos'),
+  password_confirm: yup.string().required('Confirme a senha').oneOf([yup.ref('password'), null], 'A confirmação da senha não confere')
 });
 
 export function SignUp() {
@@ -32,8 +35,16 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({name, email, password}: FormDataProps) {
-      
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post('/users', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
+
   }
 
   return (
@@ -61,7 +72,7 @@ export function SignUp() {
 
           <Controller
             control={control}
-            name='name'            
+            name='name'
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder='Nome'
@@ -71,7 +82,7 @@ export function SignUp() {
               />
             )}
           />
-          
+
           <Controller
             control={control}
             name='email'
@@ -86,7 +97,7 @@ export function SignUp() {
               />
             )}
           />
-         
+
           <Controller
             control={control}
             name='password'
