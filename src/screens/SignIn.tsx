d470ec85a82434492ce041e32ffeb5154504bd90
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2023-02-13 11:15:04 -0300

    install async storage

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,27 +1,51 @@
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 import { useAuth } from '@hooks/useAuth';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import { AppError } from '@utils/AppError';
+import { useState } from 'react';
 
+type FormData = {
+  email: string;
+  password: string;
+}
 
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const { signIn } = useAuth();
-
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const toast = useToast();
 
-  const {control, handleSubmit, formState: {errors}} = useForm<FormData>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
 
   function handleNewAccount() {
     navigation.navigate('signUp');
   }
 
   async function handleSignIn({ email, password }: FormData) {
-    await signIn(email, password);
+    try {
+      setIsLoading(true);
+      await signIn(email, password);
+
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível entrar. Tente novamente mais tarde.'
+      
+      setIsLoading(false);
+      
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+
+      
+    }
   }
 
   return (
@@ -47,20 +71,39 @@ export function SignIn() {
             Acesse sua conta
           </Heading>
 
-          <Input
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none'
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: 'Informe o e-mail' }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder='E-mail'
+                keyboardType='email-address'
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+                autoCapitalize='none'
+              />
+            )}
           />
 
-          <Input
-            placeholder='Senha'
-            secureTextEntry
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: 'Informe a senha' }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder='Senha'
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
           />
 
           <Button
             title='Acessar'
             onPress={handleSubmit(handleSignIn)}
+            isLoading={isLoading}
           />
         </Center>
 
